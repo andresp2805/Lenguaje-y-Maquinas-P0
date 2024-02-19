7d9df90398bbd9d1712818c52644e1449597ee6c
andresp2805 <a.pereiraa@uniandes.edu.co>
2024-02-19 17:02:49 -0500

    Commit Lexer Completo

--- lexer.py
+++ lexer.py
@@ -41,62 +41,25 @@ def crear_tokens(lista_tokens):
     lista3 = []
     for i in range(0,len(lista_tokens)):
         if lista_tokens[i] == '(':
-            lista.append({'LPAREN' : '('})
+            lista.append(('LPAREN' , '('))
         elif lista_tokens[i] == ')':
-            lista.append({'RPAREN' : ')'})
+            lista.append(('RPAREN' , ')'))
         elif lista_tokens[i] in COMMAND:
-            lista.append({'COMMAND' : lista_tokens[i]})
+            lista.append(('COMMAND' , lista_tokens[i]))
         elif lista_tokens[i] in CONSTANTS:
-            lista.append({'CONSTANTS' : lista_tokens[i]})
+            lista.append(('CONSTANTS' , lista_tokens[i]))
         elif lista_tokens[i] in CONTROL_STRUCTURE:
-            lista.append({'CONTROL_STRUCTURE' : lista_tokens[i]})
+            lista.append(('CONTROL_STRUCTURE' , lista_tokens[i]))
         elif lista_tokens[i] in CONDITION:
-            lista.append({'CONDITION' : lista_tokens[i]})
+            lista.append(('CONDITION' , lista_tokens[i]))
         elif lista_tokens[i] in D:
-            lista.append({'INPUT' : lista_tokens[i]})
+            lista.append(('INPUT' , lista_tokens[i]))
         elif is_a_number(lista_tokens[i]) == int:
-            lista.append({'INT: ' : lista_tokens[i]})
+            lista.append(('INT: ' , lista_tokens[i]))
         elif is_a_number(lista_tokens[i]) == float:
-            lista.append({'FLOAT' : lista_tokens[i]})
-        elif lista_tokens[i-1] == 'defun' or lista_tokens[i-1] == '=' or lista_tokens[i-1] == 'defvar':
-            lista.append({'NAME' : lista_tokens[i]})
-            lista2.append(lista_tokens[i])
-        elif lista_tokens[i] in lista2:
-            lista.append({'NAME' : lista_tokens[i]})
-        elif lista_tokens[i-3] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-4] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-5] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-6] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-7] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-8] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-9] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-10] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-11] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i-12] == 'defun':
-            lista.append({'PARAMETER' : lista_tokens[i]})
-            lista3.append(lista_tokens[i])
-        elif lista_tokens[i] in lista3:
-            lista.append({'PARAMETER' : lista_tokens[i]})
+            lista.append(('FLOAT' , lista_tokens[i]))
         else:
-            lista.append({'STR' : lista_tokens[i]})
+            lista.append(('USER PARAMETER' , lista_tokens[i]))
     return lista
 
 def is_a_number(s):
@@ -114,5 +77,4 @@ content = leer_archivo('texto.txt')
 arreglo = procesar_codigo(content)
 result = obtener_lista_tokens(arreglo)
 tokens = crear_tokens(result)
-
 print(tokens)
